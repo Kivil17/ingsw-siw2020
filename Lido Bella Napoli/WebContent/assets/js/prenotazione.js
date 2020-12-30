657fe51621503c19d0895558b400538a02d654f0
ftrot <ftrot@DESKTOP-NS4BOS1.homenet.telecomitalia.it>
2020-12-30 17:20:21 +0100

    prenotazione tavoli completata.

--- Lido Bella Napoli/WebContent/assets/js/prenotazione.js
+++ Lido Bella Napoli/WebContent/assets/js/prenotazione.js
@@ -37,3 +37,48 @@ function disponiTavoli(tavoli){
 
 	});
 }
+
+Booking.numeroPersoneW.addEventListener('click', (e) => {
+	e.preventDefault();
+	let numeroPersone = +Booking.numeroPersone.textContent;
+	if(e.target.id === 'add'){
+		Booking.numeroPersone.textContent = numeroPersone + 1;
+	}
+	else if(e.target.id === 'sub' && numeroPersone > 1){
+		Booking.numeroPersone.textContent = numeroPersone - 1;
+	}
+});
+
+Booking.tavoliW.addEventListener('click', (e) => {
+	let selezionato = +e.target.textContent;
+	if(Booking.tavoli[selezionato-1].occupato){
+		Booking.messageStatus.textContent = 'Il tavolo ${selezionato} è occupato.';
+	}
+	else{
+		Booking.tavoloSelezionato.textContent = selezionato;
+	}
+});
+
+document.forms[0].addEventListener('submit', (e) => {
+	e.preventDefault();
+	if(Booking.tavoloSelezionato.textContent == '-'){
+		Booking.messageStatus.textContent = 'E necessario selezionare un tavolo.';
+		return
+	}
+	sendBooking();
+});
+
+function sendBooking(){
+	let bookingForm = new FormData();
+	bookingForm.append('numeroPersone', +Booking.numeroPersone.textContent);
+	bookingForm.append('tavolo', +Booking.tavoloSelezionato.textContent);
+	bookingForm.append('name', document.forms[0].name.value);
+	bookingForm.append('email', document.forms[0].email.value);
+	fetch('bookingScript', {
+		body: bookingForm,
+		method: 'post'
+	});
+	Booking.messageStatus.textContent = 'La prenotazione è andata a buon fine.';
+	document.forms[0].reset();
+
+}
